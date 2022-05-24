bcfcfd8fa46ec073b33a6a9f718f694ec01c570a
shingorow <shingo.m1217@gmail.com>
2022-05-24 15:24:05 +0900

    feature: キービジュアルの調整

--- src/layouts/MainVisual.vue
+++ src/layouts/MainVisual.vue
@@ -3,6 +3,6 @@
 </script>
 
 <template>
-<div class="main-visual bg-cover bg-bottom bg-[url(/src/assets/images/keyvisuals/keyvisual_sm.png)] md:bg-[url(/src/assets/images/keyvisuals/keyvisual_md.png)] lg:bg-[url(/src/assets/images/keyvisuals/keyvisual_lg.png)] h-[50vh] min-h-[360px] md:min-h-[640px] 2xl:min-h-[960px]">
+<div class="main-visual lg:container bg-cover bg-bottom bg-[url(/src/assets/images/keyvisuals/keyvisual_sm.png)] md:bg-[url(/src/assets/images/keyvisuals/keyvisual_md.png)] lg:bg-[url(/src/assets/images/keyvisuals/keyvisual_lg.png)] h-[60vh] min-h-[360px] md:min-h-[640px] 2xl:min-h-[960px]">
 </div>
 </template>

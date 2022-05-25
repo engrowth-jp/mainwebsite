18135722c112c4ec447745da04b675bcef8f3c6f
shingorow <shingo.m1217@gmail.com>
2022-05-25 19:40:46 +0900

    deploy

--- src/layouts/Services.vue
+++ src/layouts/Services.vue
@@ -32,7 +32,7 @@ const services = [
 
                 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
                     <Box v-for="service in services" :title="service.title" :summary="service.summary"
-                        :slug="service.title" imagePath="/src/assets/images/services/" :thumbnail="service.thumbnail + '.svg'" :classes="'mt-8 md:mt-2'" />
+                        :slug="service.title" :imagePath="'/src/assets/images/services/'" :thumbnail="service.thumbnail + '.svg'" :classes="'mt-8 md:mt-2'" />
                 </div>
             </div>
         </div>

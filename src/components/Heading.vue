0f342a758652cb72e77bfb6a4445b0f3e3b560b8
shingorow <shingo.m1217@gmail.com>
2022-05-26 21:57:02 +0900

    スペーシングの変更

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -8,12 +8,12 @@ defineProps<{
 
 <template>
     <header>
-        <h2 class="text-xl pb-2 mb-3 font-heading font-bold">
+        <h2 class="text-2xl font-heading font-bold">
             <p v-if="subscript" class="text-sm">{{ subscript }}</p>
             {{ heading }}
         </h2>
 
-        <section>
+        <section class="pt-2 mt-3">
             <slot></slot>
         </section>
     </header>
